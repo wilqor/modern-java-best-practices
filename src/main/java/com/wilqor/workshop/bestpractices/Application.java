088afa93aa6f9359ba9cc08803123318dd390cf3
wilqor <wilqor@gmail.com>
2018-08-26 15:52:15 +0200

    Initialize project

---
+++ src/main/java/com/wilqor/workshop/bestpractices/Application.java
@@ -0,0 +1,11 @@
+package com.wilqor.workshop.bestpractices;
+
+/**
+ * @author wilqor
+ */
+public class Application {
+    public static void main(String[] args) {
+        var str = "It works!";
+        System.out.println(str);
+    }
+}
