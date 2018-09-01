502e72aaab3c6cd3c670e35074ae01fe84df4d9e
wilqor <wilqor@gmail.com>
2018-09-01 12:38:39 +0200

    Decrease accessibility of DietTableExportConstants

--- src/main/java/com/wilqor/workshop/bestpractices/familiar/DietTableExportConstants.java
+++ src/main/java/com/wilqor/workshop/bestpractices/familiar/DietTableExportConstants.java
@@ -3,14 +3,14 @@ package com.wilqor.workshop.bestpractices.familiar;
 /**
  * @author wilqor
  */
-public final class DietTableExportConstants {
+final class DietTableExportConstants {
     private DietTableExportConstants() {
         throw new AssertionError();
     }
 
-    public static final int COLUMNS_PER_ROW = 10;
+    static final int COLUMNS_PER_ROW = 10;
 
-    public static final int ROWS_PER_PAGE = 20;
+    static final int ROWS_PER_PAGE = 20;
 
-    public static final int PAGES_PER_EXPORT = 10_000;
+    static final int PAGES_PER_EXPORT = 10_000;
 }
