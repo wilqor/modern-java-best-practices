c24ce0abd19ed8a1b6efb391ec3e4c314f67e6d9
wilqor <wilqor@gmail.com>
2018-09-01 12:24:59 +0200

    Redo try-with-resources example

--- src/main/java/com/wilqor/workshop/bestpractices/Application.java
+++ src/main/java/com/wilqor/workshop/bestpractices/Application.java
@@ -1,20 +1,10 @@
 package com.wilqor.workshop.bestpractices;
 
-import com.wilqor.workshop.bestpractices.familiar.FoodProductLoader;
-
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.nio.file.Paths;
-
 /**
  * @author wilqor
  */
 public class Application {
-    public static void main(String[] args) throws IOException, URISyntaxException {
-        var loader = new FoodProductLoader(",");
-        URI foodProductsUri = Application.class.getResource("food_products.txt").toURI();
-        loader.loadFoodProducts(Paths.get(foodProductsUri))
-                .forEach(System.out::println);
+    public static void main(String[] args) {
+        System.out.println("It works!");
     }
 }
---
+++ src/main/java/com/wilqor/workshop/bestpractices/familiar/LineRewriter.java
@@ -0,0 +1,30 @@
+package com.wilqor.workshop.bestpractices.familiar;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.function.UnaryOperator;
+
+/**
+ * @author wilqor
+ */
+public final class LineRewriter {
+    private final UnaryOperator<String> lineConverter;
+
+    public LineRewriter(UnaryOperator<String> lineConverter) {
+        this.lineConverter = lineConverter;
+    }
+
+    public void rewriteLines(Path inputFilePath, Path outputFilePath) throws IOException {
+        try (BufferedReader reader = Files.newBufferedReader(inputFilePath);
+             BufferedWriter writer = Files.newBufferedWriter(outputFilePath)) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                writer.write(lineConverter.apply(line));
+                writer.newLine();
+            }
+        }
+    }
+}
